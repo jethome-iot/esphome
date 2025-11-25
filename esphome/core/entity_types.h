fe00794f312a5de4d6b0dc111b377007ab3548dc
Aleksey Zinchenko <zkoalexey@gmail.com>
2025-11-25 15:03:03 +0300

    [core] Add entity types and get_entity_by_key function (#26)
    - Add complete entity type enumeration
    - Add get_entity_by_key function for entity lookup
    - Enhance entity_base with type tracking
    - Update application.h with entity management

--- esphome/core/entity_types.h
+++ esphome/core/entity_types.h
@@ -6,7 +6,25 @@ namespace esphome {
 enum class EntityType : uint8_t {
   NONE,
   SWITCH,
+  BUTTON,
   SENSOR,
+  TEXT_SENSOR,
+  FAN,
+  COVER,
+  LIGHT,
+  CLIMATE,
+  NUMBER,
+  DATETIME_DATE,
+  DATETIME_TIME,
+  DATETIME_DATETIME,
+  TEXT,
+  SELECT,
+  LOCK,
+  VALVE,
+  MEDIA_PLAYER,
+  ALARM_CONTROL_PANEL,
+  EVENT,
+  UPDATE,
 };
 
 }  // namespace esphome
